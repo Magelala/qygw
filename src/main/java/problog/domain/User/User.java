83b8d20f6c48460011ae50eba49782b463c7694b
Administrator <2576047818@qq.com>
2019-08-16 19:28:21 +0800

    登录功能基本没什么问题了，sql文件已经上传

--- src/main/java/problog/domain/User/User.java
+++ src/main/java/problog/domain/User/User.java
@@ -9,6 +9,15 @@ public class User {
     private String username;
     private String password;
 
+    public User(){
+
+    }
+
+    public User(String username,String password){
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
--- src/test/java/problog/oa/OaApplicationTests.java
+++ src/test/java/problog/oa/OaApplicationTests.java
@@ -4,13 +4,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import problog.domain.User.User;
+import problog.utils.FindUser;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OaApplicationTests {
 
+    static Map<Integer, User> data;
+
+    static {
+        data = new HashMap<Integer, User>();
+        data.put(1,new User("admin","88888888"));
+        data.put(2,new User("zhangsan","123456"));
+        data.put(3,new User("lisi","00000000"));
+    }
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void test(){
+        System.out.println(FindUser.findUserByName(data,"admin"));
     }
 
 }
