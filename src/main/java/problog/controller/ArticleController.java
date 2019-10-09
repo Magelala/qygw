d329aa427ff00f1046c1e7850305542d5ead1f06
bells0 <41366442+bells0@users.noreply.github.com>
2019-10-09 11:09:09 +0800

    整合2

--- src/main/java/problog/controller/ArticleController.java
+++ src/main/java/problog/controller/ArticleController.java
@@ -114,6 +114,8 @@ public class ArticleController {
     return list;
 }
 
+
+
 }
 
 
--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,61 +1,61 @@
-var vm = new Vue({
-    el: '#content',
-    data:{
-        list:[
-            {id:1,
-            title:'halo咯',
-            category:'前端',
-            time:Date.now(),
-            operation:'删除'}
-        ]
-    },
-    router:new VueRouter({ 
-        routes:[ 
-            {path:'/',redirect:'/articleList'},
-            {path:'/articleList',
-            	component:{
-            		template: '#articleList',
-                    methods:{
-			    		selectAll:function(){
-			    			this.flag = !this.flag;
-			    		}
-			    	}
-            	}
-        	},
-            {path:'/writeArticle',
-            	component:{
-            		template:'#writeArticle',
-            		created(){
-            			layui.use('form', function(){
-							var form = layui.form;
-							form.on('submit(formDemo)', function(data){
-							layer.msg(JSON.stringify(data.field));
-							return false;
-							});
-						});
-						layui.use('upload', function(){
-							var upload = layui.upload; 
-							var uploadInst = upload.render({
-							    elem: '#test1' //绑定元素
-							    ,url: '/article/writeArticle/upload' //上传接口
-							    ,done: function(res){
-							      //上传完毕回调
-							    }
-							    ,error: function(){
-							      //请求异常回调
-							    }
-							});
-						});
-						layui.use('layedit', function(){
-							var layedit = layui.layedit;
-							layedit.build('demo'); //建立编辑器
-						});
-            		}
-            	}
-            }
-        ]
-    })
-})
+// var vm = new Vue({
+//     el: '#content',
+//     data:{
+//         list:[
+//             {id:1,
+//             title:'halo咯',
+//             category:'前端',
+//             time:Date.now(),
+//             operation:'删除'}
+//         ]
+//     },
+//     router:new VueRouter({
+//         routes:[
+//             {path:'/',redirect:'/articleList'},
+//             {path:'/articleList',
+//             	component:{
+//             		template: '#articleList',
+//                     methods:{
+// 			    		selectAll:function(){
+// 			    			this.flag = !this.flag;
+// 			    		}
+// 			    	}
+//             	}
+//         	},
+//             {path:'/writeArticle',
+//             	component:{
+//             		template:'#writeArticle',
+//             		created(){
+//             			layui.use('form', function(){
+// 							var form = layui.form;
+// 							form.on('submit(formDemo)', function(data){
+// 							layer.msg(JSON.stringify(data.field));
+// 							return false;
+// 							});
+// 						});
+// 						layui.use('upload', function(){
+// 							var upload = layui.upload;
+// 							var uploadInst = upload.render({
+// 							    elem: '#test1' //绑定元素
+// 							    ,url: '/article/writeArticle/upload' //上传接口
+// 							    ,done: function(res){
+// 							      //上传完毕回调
+// 							    }
+// 							    ,error: function(){
+// 							      //请求异常回调
+// 							    }
+// 							});
+// 						});
+// 						layui.use('layedit', function(){
+// 							var layedit = layui.layedit;
+// 							layedit.build('demo'); //建立编辑器
+// 						});
+//             		}
+//             	}
+//             }
+//         ]
+//     })
+// })
 
 		 function add(){
 	var Title = $("input[name='Title']").val();
