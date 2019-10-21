aa2c078ec129ee9b6d77414223b42a9aa80f766d
Linjiaqiang <3108136635@qq.com>
2019-10-21 16:36:42 +0800

    添加将Spring Security4默认的'X-Frame-Options' 中的 'DENY',修改为‘disable’

--- src/main/java/problog/controller/LoginController.java
+++ src/main/java/problog/controller/LoginController.java
@@ -87,6 +87,9 @@ public class LoginController {
         return "login";
     }*/
 
+
+
+
     @RequestMapping(value = "/user/login")
     public String loginPage( Map<String,Object> map){
 
--- src/main/resources/rember-me.sql
+++ src/main/resources/rember-me.sql
@@ -1,5 +1,7 @@
-#注意：这个数据表必须要创建，不得做任何修改
+/*#注意：这个数据表必须要创建，不得做任何修改
 #数据库应包含一个persistent_logins使用以下SQL（或等效SQL）创建的表：
+*/
+
 create table persistent_logins (username varchar(64) not null,
                                 series varchar(64) primary key,
                                 token varchar(64) not null,
